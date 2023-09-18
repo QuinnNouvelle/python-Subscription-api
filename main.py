1ef7ec29e55317bd346d2541170e835bc20f24a3
Ubuntu <Arbiter@TitleProServer.hr2xyabjcj5ujg5gt10sdnot4f.bx.internal.cloudapp.net>
2023-09-18 17:20:38 +0000

    Fixed issue with gunicorn loggers not logging flask logs

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.middleware.proxy_fix import ProxyFix
+import logging
 
 app = Flask(__name__)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+
+
 
 # Use ProxyFix to handle reverse proxy headers (when behind a reverse proxy)
 #app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 # Replace 'your_secret_token' with your actual secret token
 secret_token = 'SuperSecretToken'
-
 
 @app.route('/', methods=['GET'])
 def homePage():
@@ -28,14 +34,19 @@ def webhook():
 
     # Process the webhook data here (e.g., save it to a database or perform some action)
     try:
+        print("Webhook Successfully received")
+        app.logger.debug("This is a debug Message")
+        app.logger.info("Info Message")
         return jsonify({'message': 'Webhook received successfully'}), 200
 
     except Exception as e:
         print("Error:", str(e))
         return jsonify({'error': 'Internal Server Error'}), 500
 
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 
